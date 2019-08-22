cf093a20fcc352bb3f44f7c440355e711eb191d6
SharathRaparthy <sharathchandra.raparthy@gmail.com>
2019-08-22 10:40:42 -0400

    changes done

--- 05-record-trajectories-goal-babbling.py
+++ 05-record-trajectories-goal-babbling.py
@@ -2,12 +2,7 @@ import numpy as np
 import random
 from gym_ergojr.sim.single_robot import SingleRobot
 from scripts.goal_babbling import GoalBabbling
-import time
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-robot = SingleRobot(debug=False)
-
-# file_path = '/home/sharath/sim2real-record/data/freq10/'
 
 total_steps = 400 * 100
 rest_interval = 10 * 100
@@ -17,43 +12,41 @@ steps_until_resample = 100/freq
 
 #HYPERPARAMETERS
 SAMPLE_NEW_GOAL = 1
-NUMBER_OF_RETRIES = 1
+NUMBER_OF_RETRIES = 2
 ACTION_NOISE = 0.1
 K_NEAREST_NEIGHBOURS = 8
 EPSILON = 0.2
 
 goal_babbling = GoalBabbling(ACTION_NOISE, NUMBER_OF_RETRIES)
 
+# Reset the robot
+robot = SingleRobot(debug=False)
 robot.reset()
 robot.step()
-history = []
-action = np.random.uniform(-1, 1, 6)
-action[0], action[3] = 0, 0
-robot.act2(action)
-robot.step()
-achieved_goal = robot.get_tip()[0][1:]
-history.append((action, achieved_goal))
+
 end_pos = []
-goals = []
+history = []
+
+# Perform some initial random action and append to the history
+action, end_position = goal_babbling.sample_action()
+history.append((action, end_position))
+
 for epi in range(total_steps):
+    history_local = []
     if epi % SAMPLE_NEW_GOAL == 0:
         goal = [random.uniform(-0.12, 0.12), random.uniform(0.02, 0.24)]
-    if epi % rest_interval == 0:
-        print(f'Taking Rest at {epi}')
-        robot.reset()
-        robot.step()
+
     if random.random() < EPSILON:
-        action = np.random.uniform(-1, 1, 6)
+        action, end_position = goal_babbling.sample_action()
     else:
-        action = goal_babbling.inverse(goal, history)
-    action[0], action[3] = 0, 0
-    robot.act2(action)
-    robot.step()
-    end_position = robot.get_tip()[0][1:]
+        action, end_position = goal_babbling.action_retries(goal, history)
+
     history.append((action, end_position))
     end_pos.append(end_position)
 
 final_pos = np.asarray(end_pos)
+plt.scatter(final_pos[:, 0], final_pos[:, 1], alpha=0.5)
+plt.show()
 plt.hist2d(final_pos[:, 0], final_pos[:, 1], bins=100)
 plt.show()
 
--- scripts/goal_babbling.py
+++ scripts/goal_babbling.py
@@ -1,21 +1,14 @@
-import numpy as np
-import gym
-import time
 import random
+import numpy as np
 from gym_ergojr.sim.single_robot import SingleRobot
-import learners
 
 class GoalBabbling(object):
-    def __init__(self, action_noise, num_retires):
-        self._nn_set = learners.NNSet()
+    def __init__(self, action_noise, num_retries):
         self.noise = action_noise
-        self.num_retires = num_retires
+        self.retries = num_retries
         self.goal_y_range = [-0.12, 0.12]
         self.goal_z_range = [0.02, 0.24]
-
-    @staticmethod
-    def dist(a, b):
-        return sum((a_i-b_i)**2 for a_i, b_i in zip(a, b))
+        self.robot = SingleRobot(debug=True)
 
     def nearest_neighbor(self, goal, history):
         """Return the motor command of the nearest neighbor of the goal"""
@@ -23,18 +16,45 @@ class GoalBabbling(object):
         for m_command, effect in history:
             if self.dist(effect, goal) < nn_dist:
                 nn_command, nn_dist = m_command, self.dist(effect, goal)
+        nn_command[0], nn_command[3] = 0, 0
         return nn_command
 
-    def inverse(self, goals, history):
-        """Transform a goal into a motor command"""
-        nn_command = self.nearest_neighbor(goals, history)  # find the nearest neighbor of the goal.
+    def add_noise(self, nn_command):
         new_command = []
         for m_i in nn_command:
             max_i = min(1, m_i + 2 * self.noise * 1)
             min_i = max(-1, m_i + 2 * self.noise * -1)
             new_command.append(random.uniform(min_i, max_i))
+        new_command[0], new_command[3] = 0, 0
         return new_command
 
+    def sample_action(self):
+        action = np.random.uniform(-1, 1, 6)
+        action[0], action[3] = 0, 0
+        self.robot.act2(action)
+        self.robot.step()
+        end_pos = self.robot.get_tip()[0][1:]
+        return action, end_pos
+
+    def action_retries(self, goal, history):
+        history_local = []
+        action = self.nearest_neighbor(goal, history)
+        for _ in range(self.retries):
+            action_noise = self.add_noise(action)
+            self.robot.act2(action)
+            self.robot.step()
+            end_position = self.robot.get_tip()[0][1:]
+            history_local.append((action_noise, end_position))
+        action_new = self.nearest_neighbor(goal, history_local)
+        self.robot.act2(action_new)
+        self.robot.step()
+        end_pos = self.robot.get_tip()[0][1:]
+        return action_new, end_pos
+
+
+    @staticmethod
+    def dist(a, b):
+        return sum((a_i - b_i) ** 2 for a_i, b_i in zip(a, b))
 
 
 if __name__ == '__main__':
